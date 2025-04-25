1397924d4b7b067841b85630e46250deab7dd520
karim-lucky <karimccw8100@gmail.com>
2025-04-25 19:07:20 +0500

    Navbar and home section ui desing

---
+++ src/app/home/page.js
@@ -0,0 +1,15 @@
+'use client';
+
+ 
+import Link from 'next/link';
+import VpnIntroductionSection from '../components/home/VPNIntroductionSection';
+export default function HeroSection() {
+    return (
+        <div className='bg-[#F6F6F6]  '>
+            <div>
+            <VpnIntroductionSection></VpnIntroductionSection>
+            </div>
+            
+        </div>
+    );
+}
