fbee0d2b5fd64caa9479f43cbc269cd6c07c1695
altafkhankpk <145385734+altafkhankpk@users.noreply.github.com>
2025-04-28 19:14:56 +0500

    feature-our-app componet is done

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -3,11 +3,13 @@
  
 import Link from 'next/link';
 import VpnIntroductionSection from '../components/home/VPNIntroductionSection';
+import FeaturesSection from '../components/featureApp/featureforapp';
 export default function HeroSection() {
     return (
         <div className='bg-[#F6F6F6]  '>
             <div>
             <VpnIntroductionSection></VpnIntroductionSection>
+            <FeaturesSection></FeaturesSection>
             </div>
             
         </div>
