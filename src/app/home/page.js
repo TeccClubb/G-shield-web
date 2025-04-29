43dc555efca4d6eacc98a8b9c389d885efb759f2
karim-lucky <karimccw8100@gmail.com>
2025-04-29 15:20:59 +0500

    set the import of navbar and footer and sections in home

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,15 +1,16 @@
 'use client';
 
  
+import FeaturesSection from '@/components/home/featureforapp';
+import VpnIntroductionSection from '@/components/home/VPNIntroductionSection';
 import Link from 'next/link';
-import VpnIntroductionSection from '../components/home/VPNIntroductionSection';
-import FeaturesSection from '../components/featureApp/featureforapp';
+ 
 export default function HeroSection() {
     return (
         <div className='bg-zinc-400  '>
             <div>
-            <VpnIntroductionSection></VpnIntroductionSection>
-            <FeaturesSection></FeaturesSection>
+          <VpnIntroductionSection></VpnIntroductionSection>
+          <FeaturesSection></FeaturesSection>
             </div>
             
         </div>
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/navBar";
-import Footer from "./components/footer";
+
+import Navbar from "@/components/navBar";
+import Footer from "@/components/footer"; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
