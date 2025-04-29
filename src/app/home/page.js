e729ddeab816bb873d90820e4c2183651cdcaa95
altafkhankpk <145385734+altafkhankpk@users.noreply.github.com>
2025-04-29 20:02:07 +0500

    login-form are designed

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,18 +1,15 @@
 'use client';
 
  
-import DownloadProcess from '@/components/home/download-installing';
-import FeaturesSection from '@/components/home/featureforapp';
-import VpnIntroductionSection from '@/components/home/VPNIntroductionSection';
 import Link from 'next/link';
- 
+import VpnIntroductionSection from '../components/home/VPNIntroductionSection';
+import FeaturesSection from '../components/featureApp/featureforapp';
 export default function HeroSection() {
     return (
         <div className='bg-zinc-400  '>
             <div>
-          <VpnIntroductionSection></VpnIntroductionSection>
-          <FeaturesSection></FeaturesSection>
-          <DownloadProcess></DownloadProcess>
+            <VpnIntroductionSection></VpnIntroductionSection>
+            <FeaturesSection></FeaturesSection>
             </div>
             
         </div>
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -4,12 +4,14 @@ import { useState } from 'react';
 import { ChevronDown, Menu, X } from 'lucide-react';
 import Link from 'next/link';
 import { useRouter, usePathname } from 'next/navigation';
+import LoginButtonWithModal from '@/app/login-form/page';
 
 export default function Navbar() {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
     const router = useRouter();
     const pathname = usePathname(); // Get current route
+    const [isLoginOpen, setIsLoginOpen] = useState(false);
 
     return (
         <nav className={`w-full shadow-sm ${pathname === '/what-is-vpn' ? 'bg-[#F6F6F6]' : 'bg-white'}`}>
@@ -31,12 +33,16 @@ export default function Navbar() {
 
                 {/* Right CTA Button Group (Desktop) */}
                 <div className="hidden lg:flex  items-center space-x-4">
-                    <Link
-                        href="/pricing"
+                    <div
+                       onClick={() => setIsLoginOpen(true)}
                         className="px-4 py-2 text-black text-sm font-extrabold font-['Manrope'] rounded-[100px]  outline-1 outline-offset-[-1px] outline-zinc-500 inline-flex justify-center items-center gap-2.5"
                     >
                         Login
-                    </Link>
+                    </div>
+                    <LoginButtonWithModal
+                open={isLoginOpen}
+                handleClose={() => setIsLoginOpen(false)}
+            />
                     <Link
                         href="/pricing"
                         className="px-4 py-2 rounded-full bg-green-700 text-white text-sm hover:bg-green-800 transition"
