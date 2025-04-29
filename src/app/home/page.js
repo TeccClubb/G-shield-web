890fed99f8a6fe9a0a100c6c1d140ca659157e9b
karim-lucky <karimccw8100@gmail.com>
2025-04-29 20:06:48 +0500

    update the link in main home page which create the eror

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -2,14 +2,15 @@
 
  
 import Link from 'next/link';
-import VpnIntroductionSection from '../components/home/VPNIntroductionSection';
-import FeaturesSection from '../components/featureApp/featureforapp';
+ 
+ import FeaturesSection from '@/components/home/featureforapp';
+import VpnIntroductionSection from '@/components/home/VPNIntroductionSection';
 export default function HeroSection() {
     return (
         <div className='bg-zinc-400  '>
             <div>
             <VpnIntroductionSection></VpnIntroductionSection>
-            <FeaturesSection></FeaturesSection>
+             <FeaturesSection></FeaturesSection>
             </div>
             
         </div>
