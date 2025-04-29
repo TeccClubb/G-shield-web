bea009153cd5bf873fdeede521299c08a1daf3fa
karim-lucky <karimccw8100@gmail.com>
2025-04-29 20:01:59 +0500

    resolve the conflict on pushing

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,33 +1,17 @@
 'use client';
 
-
-import ForgotPasswordModal from '@/components/forgetPassword';
-import ActiveServers from '@/components/home/activeServer';
-import FeaturesSection from '@/components/home/featureforapp';
-import PricingPlans from '@/components/home/pircingPlansSection';
-import VpnIntroductionSection from '@/components/home/VPNIntroductionSection';
-import { useState } from 'react';
+ 
 import Link from 'next/link';
-
+import VpnIntroductionSection from '../components/home/VPNIntroductionSection';
+import FeaturesSection from '../components/featureApp/featureforapp';
 export default function HeroSection() {
-    const [open, setOpen] = useState(false);
     return (
         <div className='bg-zinc-400  '>
             <div>
-                <VpnIntroductionSection></VpnIntroductionSection>
-                <FeaturesSection></FeaturesSection>
-                <PricingPlans></PricingPlans>
-                <ActiveServers></ActiveServers>
-                <ForgotPasswordModal open={open} onClose={() => setOpen(false)} />
-                <button
-                    onClick={() => setOpen(true)}
-                    className="text-white   hover:text-gray-200 ml-4"
-                >
-                    Forgot Password?
-                </button>
-
+            <VpnIntroductionSection></VpnIntroductionSection>
+            <FeaturesSection></FeaturesSection>
             </div>
-
-        </div>
-    );
+            
+        </div>
+    );
 }
