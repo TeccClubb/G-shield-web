a5701f71378f72f31870b2d952ebc7fac21a946a
karim-lucky <karimccw8100@gmail.com>
2025-04-29 19:49:10 +0500

    ForgetPassord modal ui desing and ui desing the sctiveServer and PricingplaneSection in home page

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,18 +1,33 @@
 'use client';
 
- 
+
+import ForgotPasswordModal from '@/components/forgetPassword';
+import ActiveServers from '@/components/home/activeServer';
 import FeaturesSection from '@/components/home/featureforapp';
+import PricingPlans from '@/components/home/pircingPlansSection';
 import VpnIntroductionSection from '@/components/home/VPNIntroductionSection';
+import { useState } from 'react';
 import Link from 'next/link';
- 
+
 export default function HeroSection() {
+    const [open, setOpen] = useState(false);
     return (
         <div className='bg-zinc-400  '>
             <div>
-          <VpnIntroductionSection></VpnIntroductionSection>
-          <FeaturesSection></FeaturesSection>
+                <VpnIntroductionSection></VpnIntroductionSection>
+                <FeaturesSection></FeaturesSection>
+                <PricingPlans></PricingPlans>
+                <ActiveServers></ActiveServers>
+                <ForgotPasswordModal open={open} onClose={() => setOpen(false)} />
+                <button
+                    onClick={() => setOpen(true)}
+                    className="text-white   hover:text-gray-200 ml-4"
+                >
+                    Forgot Password?
+                </button>
+
             </div>
-            
+
         </div>
     );
 }
