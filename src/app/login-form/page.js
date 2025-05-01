9c111c9de9a306e3322400b182c6823c1158d02c
altafkhankpk <145385734+altafkhankpk@users.noreply.github.com>
2025-05-01 16:42:52 +0500

    signup and login page validation are completed

--- src/app/login-form/page.js
+++ src/app/login-form/page.js
@@ -18,14 +18,12 @@ import {
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import Image from "next/image";
-<<<<<<< Updated upstream
-=======
 import ForgotPasswordModal from "@/components/forgetPassword";
->>>>>>> Stashed changes
 
 export default function LoginButtonWithModal({ open, handleClose }) {
     const [tab, setTab] = useState(0);
     const handleTabChange = (_, newValue) => setTab(newValue);
+    const [OpenForget, setForgetOpen] = useState(false);
 
     const { control, handleSubmit, formState: { errors } } = useForm();
 
@@ -39,51 +37,9 @@ export default function LoginButtonWithModal({ open, handleClose }) {
             console.log("Create Account Data:", data);
         }
     };
+    
 
     return (
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        <Dialog
-            open={open}
-            onClose={handleClose}
-            fullWidth
-            maxWidth="sm"
-            sx={{ "& .MuiDialog-paper": { maxHeight: '98vh' } }}
-        >
-            <Box display="flex" justifyContent="space-between" alignItems="center" px={2} pt={2}>
-                <Image
-                    src="/Log-icon/G-VPN-removebg-preview 1.png"
-                    alt="GShield Logo"
-                    width={40}
-                    height={40}
-                />
-                <IconButton onClick={handleClose}>
-                    <CloseIcon />
-                </IconButton>
-            </Box>
-
-            <Tabs
-                value={tab}
-                onChange={handleTabChange}
-                centered
-                textColor="inherit"
-                TabIndicatorProps={{
-                    style: {
-                        backgroundColor: "green", // Tab indicator color
-                    },
-                }}
-                sx={{
-                    "& .MuiTab-root": {
-                        color: "gray", // Default text color
-                    },
-                    "& .Mui-selected": {
-                        color: "black",
-                        fontWeight: "bold" // Selected tab text color
-                    },
-                }}
-=======
-=======
->>>>>>> Stashed changes
         <>
             <Dialog
                 open={open}
@@ -91,97 +47,6 @@ export default function LoginButtonWithModal({ open, handleClose }) {
                 fullWidth
                 maxWidth="sm"
                 sx={{ "& .MuiDialog-paper": { maxHeight: '98vh' } }}
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-            >
-                <Tab label="Sign In" />
-                <Tab label="Create an account" />
-            </Tabs>
-
-
-            <DialogContent sx={{ pt: 1, px: 10 }}>
-                <Typography variant="h6" fontWeight="bold" mb={2}>
-                    {tab === 0 ? "Welcome Back" : "Create Your Account"}
-                </Typography>
-
-
-                <TextField
-                    variant="standard"
-                    label="Email or Username"
-                    fullWidth
-                    margin="dense"
-                    type="email"
-                    defaultValue="jovanca@gmail.com"
-                    InputProps={{
-                        endAdornment: (
-                            <InputAdornment position="end">
-                                <Image
-                                    src="/Log-icon/email.svg"
-                                    alt="Email Icon"
-                                    width={20}
-                                    height={15}
-                                />
-                            </InputAdornment>
-                        ),
-                    }}
-                />
-
-                <TextField
-                    variant="standard"
-                    label="Password"
-                    fullWidth
-                    margin="dense"
-                    type="password"
-                    defaultValue="**********"
-                    InputProps={{
-                        endAdornment: (
-                            <InputAdornment position="end">
-                                <Image
-                                    src="/Log-icon/privatePassword.svg"
-                                    alt="Password Icon"
-                                    width={20}
-                                    height={15}
-                                />
-                            </InputAdornment>
-                        ),
-                    }}
-                />
-
-                <Box display="flex" justifyContent="space-between" alignItems="center" my={1}>
-                    <FormControlLabel
-                        sx={{ color: "gray" }}
-                        control={
-                            <Checkbox
-                                defaultChecked
-                                sx={{
-                                    color: "green", // unchecked color
-                                    '&.Mui-checked': {
-                                        color: "green", // checked color
-                                    }
-                                }}
-                            />
-                        }
-                        label="Remember Me"
-                    />
-                    <Button size="small" sx={{ color: "green" }}>
-                        Forgot Password
-                    </Button>
-                </Box>
-
-<<<<<<< Updated upstream
-
-                <Button
-                    variant="contained"
-                    color="success"
-                    fullWidth
-                    sx={{ borderRadius: "8px", textTransform: "none", mb: 1 }}
-                >
-                    Login
-                </Button>
-
-                <Divider sx={{ color: "gray" }}>Instant Login</Divider>
-=======
-=======
             >
                 <Box display="flex" justifyContent="space-between" alignItems="center" px={2} pt={2}>
                     <Image
@@ -195,77 +60,12 @@ export default function LoginButtonWithModal({ open, handleClose }) {
                     </IconButton>
                 </Box>
 
->>>>>>> Stashed changes
                 <Tabs
                     value={tab}
                     onChange={handleTabChange}
                     centered
                     textColor="inherit"
-<<<<<<< Updated upstream
-                    TabIndicatorProps={{
-                        style: {
-                            backgroundColor: "green", // Tab indicator color
-                        },
-                    }}
-                    sx={{
-                        "& .MuiTab-root": {
-                            color: "gray", // Default text color
-                        },
-                        "& .Mui-selected": {
-                            color: "black",
-                            fontWeight: "bold" // Selected tab text color
-                        },
-                    }}
-                >
-                    <Tab label="Sign In" />
-                    <Tab label="Create an account" />
-                </Tabs>
-
-
-                <DialogContent sx={{ pt: 1, px: 10 }}>
-                    <Typography variant="h6" fontWeight="bold" mb={2}>
-                        {tab === 0 ? "Welcome Back" : "Create Your Account"}
-                    </Typography>
-
-
-                    <TextField
-                        variant="standard"
-                        label="Email or Username"
-                        fullWidth
-                        margin="dense"
-                        type="email"
-                        defaultValue="jovanca@gmail.com"
-                        InputProps={{
-                            endAdornment: (
-                                <InputAdornment position="end">
-                                    <Image
-                                        src="/Log-icon/email.svg"
-                                        alt="Email Icon"
-                                        width={20}
-                                        height={15}
-                                    />
-                                </InputAdornment>
-                            ),
-                        }}
-                    />
->>>>>>> Stashed changes
-
-
-<<<<<<< Updated upstream
-                <Button
-                    fullWidth
-                    variant="outlined"
-                    startIcon={
-                        <Image
-                            src="/Log-icon/Google__G__Logo (1) 1.svg"
-                            alt="Google Logo"
-                            width={20}
-                            height={20}
-                        />
-                    }
-=======
                     TabIndicatorProps={{ style: { backgroundColor: "green" } }}
->>>>>>> Stashed changes
                     sx={{
                         "& .MuiTab-root": { color: "gray" },
                         "& .Mui-selected": { color: "black", fontWeight: "bold" },
@@ -419,89 +219,18 @@ export default function LoginButtonWithModal({ open, handleClose }) {
                                 height={20}
                             />
                         }
-<<<<<<< Updated upstream
-                    }}
-                >
-                    Continue with Apple
-                </Button>
-            </DialogContent>
-        </Dialog>
-=======
-                    <Box display="flex" justifyContent="space-between" alignItems="center" my={1}>
-                        <FormControlLabel
-                            sx={{ color: "gray" }}
-                            control={
-                                <Checkbox
-                                    defaultChecked
-                                    sx={{
-                                        color: "green", // unchecked color
-                                        '&.Mui-checked': {
-                                            color: "green", // checked color
-                                        }
-                                    }}
-                                />
-                            }
-                            label="Remember Me"
-                        />
-                        <Button size="small" onClick={() => {
-                                handleClose();         // Close login modal
-                                setForgetOpen(true);   // Open forget password modal
-                            }} sx={{ color: "green" }}>
-                            Forgot Password
-                        </Button>
-                    </Box>
-
-
-                    <Button
-                        variant="contained"
-                        color="success"
-                        fullWidth
-                        sx={{ borderRadius: "8px", textTransform: "none", mb: 1 }}
-                    >
-                        Login
-                    </Button>
-
-                    <Divider sx={{ color: "gray" }}>Instant Login</Divider>
-
-
-                    <Button
-                        fullWidth
-                        variant="outlined"
-                        startIcon={
-                            <Image
-                                src="/Log-icon/Google__G__Logo (1) 1.svg"
-                                alt="Google Logo"
-                                width={20}
-                                height={20}
-                            />
-                        }
-=======
->>>>>>> Stashed changes
                         sx={{
                             mt: 1,
                             borderRadius: "8px",
                             textTransform: "none",
-<<<<<<< Updated upstream
-                            color: "green", // set text color
-                            borderColor: "gray", // optional: to match border color
-                            '&:hover': {
-                                borderColor: 'gray',
-                                color: 'gray',
-                            }
-=======
                             color: "green",
                             borderColor: "gray",
                             '&:hover': { borderColor: 'gray', color: 'gray' },
->>>>>>> Stashed changes
                         }}
                     >
                         Continue with Google
                     </Button>
 
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
                     <Button
                         fullWidth
                         variant="outlined"
@@ -517,34 +246,20 @@ export default function LoginButtonWithModal({ open, handleClose }) {
                             mt: 1,
                             borderRadius: "8px",
                             textTransform: "none",
-<<<<<<< Updated upstream
-                            color: "green", // set text color
-                            borderColor: "gray", // optional: to match border color
-                            '&:hover': {
-                                borderColor: 'gray',
-                                color: 'gray',
-                            }
-=======
                             color: "green",
                             borderColor: "gray",
                             '&:hover': { borderColor: 'gray', color: 'gray' },
->>>>>>> Stashed changes
                         }}
                     >
                         Continue with Apple
                     </Button>
                 </DialogContent>
             </Dialog>
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
             <ForgotPasswordModal
                 open={OpenForget}
                 onClose={() => setForgetOpen(false)}
             />
         </>
-
->>>>>>> Stashed changes
     );
 }
